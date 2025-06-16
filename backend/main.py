d1164dccb3b05a3ea80daf955aaa1e42eff9ffd4
Varoon Gupta <varoongupta@Varoons-MBP.ht.home>
2025-06-16 11:41:10 -0400

    fix CORS error

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
-        "https://prompt-rx.vercel.app/"
+        "https://prompt-rx.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
